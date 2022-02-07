748b9cd9cd27d2f5668a3916ce6a20970d21b5cb
Hernan Menajovsky <hmenajovsky@gmail.com>
2022-02-07 16:24:47 +0100

    updated users route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,14 @@ const router = require("express").Router();
 const Users = require("./../models/User.model");
 const mongoose = require("mongoose");
 
-router.get("/:role", (req, res, next) => {
-    Users.find({ role: { $ne: req.params.role} })
+router.get("/nanny", (req, res, next) => {
+    Users.find({ role: { $eq: "nanny"} })
+    .then((users) => res.send(users))
+    .catch(next);
+});
+
+router.get("/family", (req, res, next) => {
+    Users.find({ role: { $eq: "family"} })
     .then((users) => res.send(users))
     .catch(next);
 });
